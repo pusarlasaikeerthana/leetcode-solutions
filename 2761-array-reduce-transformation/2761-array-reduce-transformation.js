c66c8859bca287c2a0f92b22a8cb67b7d538d2e8
pusarlasaikeerthana <90471987+pusarlasaikeerthana@users.noreply.github.com>
2025-04-25 23:35:23 +0530

    Time: 47 ms (37.35%), Space: 49.5 MB (100%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+
+    
+    var res = init
+    for(var i=0; i<nums.length;i++){
+        res = fn(res,nums[i])
+    }
+    return res    
+};
